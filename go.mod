39a227b5bd3df94ad8f425cc24e092b0def3af09
Anton K <plan.kapkan@outlook.com>
2022-05-15 16:36:23 +0300

    lesson 4 end

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module plankapkan/study
 
 go 1.18
+
+require github.com/mitchellh/mapstructure v1.5.0
--- lesson4/lesson4.go
+++ lesson4/lesson4.go
@@ -1,46 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/mitchellh/mapstructure"
+)
 
 type Point struct {
-	X int
-	Y int
+	X int //`mapstructure:"xx"`
+	Y int `mapstructure:"yy"`
+}
+
+func (p Point) method() {
+	fmt.Println("coordinates are", "x", p.X, "y", p.Y)
 }
 
 func main() {
-	// arr := []string{"a", "b", "c"}
-	// for index, element := range arr { // index + element
-	// 	fmt.Println(index)
-	// 	fmt.Println(element)
-	// }
-	// for _, element := range arr { // _ to skip index
-	// 	fmt.Println(element)
-	// }
-	pointMap := map[string]Point{
-		"b": {
-			X: 11,
-			Y: 13,
-		},
+	pointMap := map[string]int{
+		"x":  11,
+		"yy": 14,
 	}
-	otherMap := make(map[int]Point)
-	// var oneMoreOtherMap map[string]Point
-	pointMap["a"] = Point{
-		X: 0,
-		Y: 1,
-	}
-	// fmt.Println(pointMap)
-	// fmt.Println(pointMap["a"])
-	otherMap[1] = Point{3, 4}
-	// fmt.Println(otherMap)
-	// fmt.Println(otherMap[1])
+	pointStruct := Point{}
+	mapstructure.Decode(pointMap, &pointStruct)
+	fmt.Println(pointStruct)
 
-	key := "c"
-	value, isok := pointMap[key]
-	if isok {
-		fmt.Printf("key %s exists\n", key)
-		fmt.Printf("value is %d", value)
-	} else {
-		fmt.Printf("key %s not exist", key)
+	for k, v := range pointMap {
+		fmt.Println(k)
+		fmt.Println(v)
 	}
-
 }
